1ab0a5082472899f785c992dd934a6450a300ca5
ArthurD <arthur.docquois@wanadoo.fr>
2024-04-20 13:13:09 +0200

    Ajout du mode fullscreen

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import tkinter as tk
 import tkintermapview as tkmap
 from PIL import ImageTk, Image
-
 class App(tk.Tk):
     
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self,*args, **kwargs)
         container = tk.Frame(self)
         self.title("Map")
-        self.geometry(f"{800}x{600}")
+        self.attributes('-fullscreen', True)
+        self.fullScreenState=True
+        print(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}")
+        self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}")
         container.pack(side="top", fill="both", expand= True)
 
         container.grid_rowconfigure(0, weight=1)
@@ -30,7 +32,7 @@ class App(tk.Tk):
 class Map(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        map_widget=tkmap.TkinterMapView(self, width=800,height=600, corner_radius=0)
+        map_widget=tkmap.TkinterMapView(self, width=parent.winfo_screenwidth(),height=parent.winfo_screenheight(), corner_radius=0)
         map_widget.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
         map_widget.set_position(48.653103594064795, -2.356723508882328)
