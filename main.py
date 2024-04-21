e8902bf7974f8a44496e6cede8bd2c5664651d96
ArthurD <arthur.docquois@wanadoo.fr>
2024-04-21 13:07:28 +0200

    Full screen image

--- main.py
+++ main.py
@@ -42,35 +42,53 @@ class Map(tk.Frame):
         map_widget.set_zoom(15)
         marker_1 = map_widget.set_marker(48.68321841376174, -2.3191363028489853, text="Cap Frehel", command=self.show_image)
 
+
     def show_image(self, marker):
-        photo = Photo(self, self.parent)
-        photo.place(in_=self,anchor=tk.CENTER,relx=0.5,rely=0.5)
+        self.photo = Photo(self, self.parent)
+        self.photo.place(in_=self,anchor=tk.CENTER,relx=0.5,rely=0.5)
         print(f"Opening : {marker.text}")
+
+    def map_clicked(self,event):
+        print("Map clicked")
 
 class Photo(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
         self.parent = parent
         image = Image.open("./photo/1/IMG_1980.jpeg")
-        w, h = self.image_size(image)
+        w, h = self.image_size(image,2)
         photo = ImageTk.PhotoImage(image.resize((w,h)))
         self.photoLabel = tk.Label(self, image=photo)                                          
         self.photoLabel.image = photo
         self.photoLabel.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        self.photoLabel.bind("<Button-1>", self.photo_clicked)
         self.photoLabel.pack()
 
-    def image_size(self, image):
+    def image_size(self, image, coeff):
         imgw, imgh = image.size
         windw, windh = self.parent.winfo_width(), self.parent.winfo_height()
-        if imgw < windw//2 or imgh < windh//2:
+        if imgw < windw//coeff or imgh < windh//coeff:
             return imgw, imgh 
         else :
-            ratiow = imgw//(windw//2)
-            ratioh = imgh//(windh//2)
+            ratiow = imgw//(windw//coeff)
+            ratioh = imgh//(windh//coeff)
             max_ratio = max(ratiow, ratioh)
             return imgw//max_ratio, imgh//max_ratio
 
-
+    def photo_clicked(self, event):
+        print("Photo clicked")
+        image = Image.open("./photo/1/IMG_1980.jpeg")
+        w, h = self.image_size(image,1)
+        photo = ImageTk.PhotoImage(image.resize((w,h)))
+        self.photoLabel.destroy() 
+        self.photoLabel = tk.Label(self, image=photo)
+        self.photoLabel.image = photo
+        self.photoLabel.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        self.photoLabel.bind("<Button-1>", self.full_screen_photo_click)
+        self.photoLabel.pack() 
+    
+    def full_screen_photo_click(self, event):
+        self.destroy()
 
 
 if __name__ == '__main__':
