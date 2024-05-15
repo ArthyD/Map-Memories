f2317083d3a49439db15bd0a3724edd386dc6eaf
ArthurD <arthur.docquois@wanadoo.fr>
2024-05-15 18:54:28 +0200

    Changement listening address

--- main_web.py
+++ main_web.py
@@ -7,7 +7,7 @@ from multiprocessing import Process
 if __name__ == '__main__':
     app = create_app()
     app.app_context().push()
-    app.run(debug=True, port = 8080, host='127.0.0.1')
+    app.run(debug=True, port = 8080, host='0.0.0.0')
     
     
     
